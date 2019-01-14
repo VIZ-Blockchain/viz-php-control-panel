e49313af5e15acd2994028ae26fe4f97aa8b7b4f
Anatoly Piskunov <anatoly.piskunov@gmail.com>
2019-01-14 10:41:47 +0000

    Add award calc in links plugin

--- module/plugins/links.php
+++ module/plugins/links.php
@@ -1,5 +1,101 @@
 <?php
 class viz_plugin_links extends viz_plugin{
+	function award($info,$data){
+		global $config;
+		if(in_array('content',$config['plugins'])){
+			if(in_array('votes',$config['plugins_extensions']['links'])){
+				$memo=$data['memo'];
+				if(false!==strpos($memo,'/')){
+					$author=substr($memo,0,strpos($memo,'/'));
+					if($data['receiver']==$author){
+						$permlink=substr($memo,strpos($memo,'/')+1);
+						$weight=(int)$data['energy'];
+						$author_id=get_user_id($author);
+						print '$author='.$author.'/$permlink='.$permlink.PHP_EOL;
+						if($author_id){
+							$user_login=$data['initiator'];
+							$user_id=get_user_id($user_login);
+							if($user_id){
+								if(in_array('users',$config['plugins'])){
+									redis_add_ulist('update_user',$user_login);
+									$this->redis->zadd('users_action_time',$info['unixtime'],$user_login);
+								}
+								$find_content=mongo_find_id('content',array('author'=>(int)$author_id,'permlink'=>mongo_prepare($permlink)));
+								if($find_content){
+									$find_content_vote=mongo_find_id('content_votes',['parent'=>(int)$find_content,'user'=>(int)$user_id]);
+									if($find_content_vote){
+										$bulk=new MongoDB\Driver\BulkWrite;
+										$bulk->update(['_id'=>(int)$find_content_vote],['$set'=>['weight'=>(int)$weight,'time'=>(int)$info['unixtime']]]);
+										$this->mongo->executeBulkWrite($config['db_prefix'].'.content_votes',$bulk);
+									}
+									else{
+										$link_arr=array('_id'=>(int)mongo_counter('content_votes',true),'parent'=>(int)$find_content,'author'=>(int)$author_id,'user'=>(int)$user_id,'weight'=>(int)$weight,'time'=>(int)$info['unixtime']);
+										$bulk=new MongoDB\Driver\BulkWrite;
+										$bulk->insert($link_arr);
+										$this->mongo->executeBulkWrite($config['db_prefix'].'.content_votes',$bulk);
+									}
+									unset($find_content);
+								}
+								else{
+									$find_subcontent=mongo_find_id('subcontent',array('author'=>(int)$author_id,'permlink'=>mongo_prepare($permlink)));
+									if($find_subcontent){
+										$find_subcontent_vote=mongo_find_id('subcontent_votes',['parent'=>(int)$find_subcontent,'user'=>(int)$user_id]);
+										if($find_subcontent_vote){
+											$bulk=new MongoDB\Driver\BulkWrite;
+											$bulk->update(['_id'=>(int)$find_subcontent_vote],['$set'=>['weight'=>(int)$weight,'time'=>(int)$info['unixtime']]]);
+											$this->mongo->executeBulkWrite($config['db_prefix'].'.subcontent_votes',$bulk);
+										}
+										else{
+											$link_arr=array('_id'=>(int)mongo_counter('subcontent_votes',true),'parent'=>(int)$find_subcontent,'author'=>(int)$author_id,'user'=>(int)$user_id,'weight'=>(int)$weight,'time'=>(int)$info['unixtime']);
+											$bulk=new MongoDB\Driver\BulkWrite;
+											$bulk->insert($link_arr);
+											$this->mongo->executeBulkWrite($config['db_prefix'].'.subcontent_votes',$bulk);
+										}
+										unset($find_content);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	function receive_award($info,$data){
+		global $config;
+		if(in_array('content',$config['plugins'])){
+			if(in_array('votes',$config['plugins_extensions']['links'])){
+				$memo=$data['memo'];
+				if(false!==strpos($memo,'/')){
+					$author=substr($memo,0,strpos($memo,'/'));
+					if($data['receiver']==$author){
+						$permlink=substr($memo,strpos($memo,'/')+1);
+						$author_id=get_user_id($author);
+						if($author_id){
+							$find_content=mongo_find_id('content',array('author'=>(int)$author_id,'permlink'=>mongo_prepare($permlink)));
+							if($find_content){
+								$award_shares=(int)(floatval($data['shares'])*1000000);
+								$bulk=new MongoDB\Driver\BulkWrite;
+								$bulk->update(['_id'=>(int)$find_content],['$inc'=>['receive_award'=>$award_shares]]);
+								$this->mongo->executeBulkWrite($config['db_prefix'].'.content',$bulk);
+								unset($find_content);
+							}
+							else{
+								$find_subcontent=mongo_find_id('subcontent',array('author'=>(int)$author_id,'permlink'=>mongo_prepare($permlink)));
+								if($find_subcontent){
+									$award_shares=(int)(floatval($data['shares'])*1000000);
+									$bulk=new MongoDB\Driver\BulkWrite;
+									$bulk->update(['_id'=>(int)$find_subcontent],['$inc'=>['receive_award'=>$award_shares]]);
+									$this->mongo->executeBulkWrite($config['db_prefix'].'.content',$bulk);
+									unset($find_content);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 	function vote($info,$data){
 		global $config;
 		if(in_array('content',$config['plugins'])){
